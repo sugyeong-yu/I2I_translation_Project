23e6763822845066f0745b51b47b2b115a37efb3
ysg <tnrud7495@gmail.com>
2021-06-11 11:30:23 +0900

    s

--- Code/stargan/model.py
+++ Code/stargan/model.py
@@ -1,88 +1,70 @@
-import torch
+import torch , torchvision
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 
-
-class ResidualBlock(nn.Module):
-    """Residual Block with instance normalization."""
-    def __init__(self, dim_in, dim_out):
-        super(ResidualBlock, self).__init__()
-        self.main = nn.Sequential(
-            nn.Conv2d(dim_in, dim_out, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.InstanceNorm2d(dim_out, affine=True, track_running_stats=True),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(dim_out, dim_out, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.InstanceNorm2d(dim_out, affine=True, track_running_stats=True))
-
-    def forward(self, x):
-        return x + self.main(x)
-
-
 class Generator(nn.Module):
-    """Generator network."""
-    def __init__(self, conv_dim=64, c_dim=5, repeat_num=6):
-        super(Generator, self).__init__()
+    def __init__(self, conv_dim=64, c_dim=5, repeat_num):
+        super(Generator,self).__init__()
 
-        layers = []
-        layers.append(nn.Conv2d(3+c_dim, conv_dim, kernel_size=7, stride=1, padding=3, bias=False))
-        layers.append(nn.InstanceNorm2d(conv_dim, affine=True, track_running_stats=True))
-        layers.append(nn.ReLU(inplace=True))
+        self.conv1 = nn.Conv2d(3+c_dim,conv_dim,kernel_size=7,stride=1,padding=3)
+        self.InsNorm1 = nn.InstanceNorm2d(conv_dim,affine=True) # affine True : 매개변수도 학습가능하게 해줌
+        self.activation=nn.ReLU(inplace=True)# inplace : input으로 들어온것 자체를 수정하겠다는 뜻?
 
-        # Down-sampling layers.
-        curr_dim = conv_dim
-        for i in range(2):
-            layers.append(nn.Conv2d(curr_dim, curr_dim*2, kernel_size=4, stride=2, padding=1, bias=False))
-            layers.append(nn.InstanceNorm2d(curr_dim*2, affine=True, track_running_stats=True))
-            layers.append(nn.ReLU(inplace=True))
-            curr_dim = curr_dim * 2
+        self.conv2 = nn.Conv2d(conv_dim,conv_dim*2,kernel_size=4,stride=2,padding=1)
+        self.InsNorm2 = nn.InstanceNorm2d(conv_dim*2,affine=True)
 
-        # Bottleneck layers.
-        for i in range(repeat_num):
-            layers.append(ResidualBlock(dim_in=curr_dim, dim_out=curr_dim))
+        self.conv3 = nn.Conv2d(conv_dim*2,conv_dim*4,kernel_size=4,stride=2,padding=1)
+        self.InsNorm3 = nn.InstanceNorm2d(conv_dim*4,affine=True)
 
-        # Up-sampling layers.
-        for i in range(2):
-            layers.append(nn.ConvTranspose2d(curr_dim, curr_dim//2, kernel_size=4, stride=2, padding=1, bias=False))
-            layers.append(nn.InstanceNorm2d(curr_dim//2, affine=True, track_running_stats=True))
-            layers.append(nn.ReLU(inplace=True))
-            curr_dim = curr_dim // 2
+        self.Downsampling = nn.Sequential(self.conv1,self.InsNorm1,self.activation,
+                                          self.conv2,self.InsNorm2,self.activation,
+                                          self.conv3,self.InsNorm3,self.activation)
 
-        layers.append(nn.Conv2d(curr_dim, 3, kernel_size=7, stride=1, padding=3, bias=False))
-        layers.append(nn.Tanh())
-        self.main = nn.Sequential(*layers)
+        self.bottleneck = Residual_block(in_dim,repeat_num)
 
-    def forward(self, x, c):
-        # Replicate spatially and concatenate domain information.
-        # Note that this type of label conditioning does not work at all if we use reflection padding in Conv2d.
-        # This is because instance normalization ignores the shifting (or bias) effect.
-        c = c.view(c.size(0), c.size(1), 1, 1)
-        c = c.repeat(1, 1, x.size(2), x.size(3))
-        x = torch.cat([x, c], dim=1)
-        return self.main(x)
 
+        self.deconv1 = nn.ConvTranspose2d(conv_dim*4,conv_dim*2,kernel_size=4,stride=2,padding=1)
+        self.InsNorm4 = nn.InstanceNorm2d(conv_dim*2,affine=True)
+
+        self.deconv2 = nn.ConvTranspose2d(conv_dim*2,conv_dim,kernel_size=4,stride=2,padding=1)
+        self.InsNorm5 = nn.InstanceNorm2d(conv_dim,affine=True)
+
+        self.conv4 = nn.Conv2d(conv_dim,3+c_dim,kernel_size=7,stride=1,padding=3)
+        self.tanh = nn.Tanh()
+
+        self.Upsampling=nn.Sequential(self.deconv1,self.InsNorm4,self.activation,
+                                      self.deconv2,self.InsNorm5,self.activation,
+                                      self.conv4,self.tanh)
+
+    def forward(self,x,c):
+        c = c.view(c.size(0),c.size(1),1,1)
+        c = c.repeat(1,1,x.size(2),x.size(3))
+        x = torch.cat([x,c],dim=1)
+
+        G = nn.Sequential(self.Downsampling,self.bottleneck,self.Upsampling)
+        return G(x)
 
 class Discriminator(nn.Module):
-    """Discriminator network with PatchGAN."""
-    def __init__(self, image_size=128, conv_dim=64, c_dim=5, repeat_num=6):
-        super(Discriminator, self).__init__()
-        layers = []
-        layers.append(nn.Conv2d(3, conv_dim, kernel_size=4, stride=2, padding=1))
-        layers.append(nn.LeakyReLU(0.01))
+    def __init__(self,img_size,conv_dim=64,c_dim=5,repeat_num):
+        layers=[]
+        layers.append(nn.Conv2d(3,conv_dim,kernel_size=4,stride=2,padding=1))
+        layers.append(nn.LeakyReLU(inplace=True))
+        #hidden layer
+        d=conv_dim
+        for i in range(repeat_num):
+            layers.append(nn.Conv2d(d,d*2,kernel_size=4,stride=2,padding=1))
+            layers.append(nn.LeakyReLU(inplace=True))
+            d*=2
 
-        curr_dim = conv_dim
-        for i in range(1, repeat_num):
-            layers.append(nn.Conv2d(curr_dim, curr_dim*2, kernel_size=4, stride=2, padding=1))
-            layers.append(nn.LeakyReLU(0.01))
-            curr_dim = curr_dim * 2
+        self.main=nn.Sequential(*layers)
 
-        kernel_size = int(image_size / np.power(2, repeat_num))
-        self.main = nn.Sequential(*layers)
-        self.conv1 = nn.Conv2d(curr_dim, 1, kernel_size=3, stride=1, padding=1, bias=False)
-        self.conv2 = nn.Conv2d(curr_dim, c_dim, kernel_size=kernel_size, bias=False)
-        
-    def forward(self, x):
-        h = self.main(x)
-        out_src = self.conv1(h)
-        out_cls = self.conv2(h)
-        return out_src, out_cls.view(out_cls.size(0), out_cls.size(1))
+        #output layer
+        self.src_d=nn.Conv2d(d,1,kernel_size=3,stride=1,padding=1)
+        self.cls_d=nn.Conv2d(d,c_dim,kernel_size=int(img_size/64))
+
+    def forward(self,x):
+        h=self.main(x)
+        out_src=self.src_d(h)
+        out_cls=self.cls_d(h)
+        return out_src,out_cls.view(out_cls.size(0),out_cls.size(1))
